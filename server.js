331e03cb157fa7246b8e72d77fe2e2f9f105ed23
Gad Nadjar <gadnadjar@gmail.com>
2025-01-14 18:22:44 +0200

    add new model user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 //Middlewares
 app.use(express.json())
 app.use(cors());
+await connectDB();
 
 
 app.get('/', (req, res) => {
@@ -18,3 +20,8 @@ app.get('/', (req, res) => {
 
 
 app.listen(PORT, () => console.log('Server Running On port ' + PORT));
+
+
+//gadnadjar
+//pYjwUaGZBG7gg7po
+//mongodb+srv://gadnadjar:<db_password>@cluster0.rqlnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
