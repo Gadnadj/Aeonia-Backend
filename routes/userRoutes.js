bf95f5d7e732e27a021c54a29e073a6df99c4643
Gad Nadjar <gadnadjar@gmail.com>
2025-01-15 11:54:25 +0200

    remove transaction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, userCredit } from '../controllers/userController.js';
+import { registerUser, loginUser, userCredit, paymentRazorpay } from '../controllers/userController.js';
 import userAuth from '../middlewares/auth.js';
 
 const userRouter = express.Router();
@@ -7,7 +7,6 @@ const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/credits', userAuth, userCredit);
-
 
 export default userRouter;
 
